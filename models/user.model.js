0d4df77881c47c82470204486907ae08aecd78ef
Webster-Butterfly <troymay0901@gmail.com>
2017-03-21 10:12:04 +0100

    exercice

--- models/user.model.js
+++ models/user.model.js
@@ -7,13 +7,6 @@ class UserModel {
     this.description = description;
   }
 
-  getName() {
-    if(this.firstName.length < 2 || this.lastName.length < 2) return `${this.firstName} ${this.lastName}`;
-    const first = this.firstName.charAt(0).toUpperCase() + this.firstName.substring(1);
-    const last = this.lastName.charAt(0).toUpperCase() + this.lastName.substring(1);
-    return `${first} ${last}`;
-  }
-
 }
 
 module.exports = UserModel;
